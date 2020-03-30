a768f1af3bc2e509d9df79d4bd7d2ad97ef8750b
Chris Scott <chris@iamzed.com>
2020-03-29 21:18:32 -0400

    Change to Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
-import {
-  Container, Header, Button, Icon,
-} from 'semantic-ui-react';
-import 'semantic-ui-less/semantic.less';
-import getHaiku from './generator/haiku';
 
-const mobile = false;
+import {
+  Typography, Container, Grid, Button, Card, CardContent,
+} from '@material-ui/core';
+import getHaiku from './generator/haiku';
 
 const App = (): JSX.Element => {
   const [haiku, setHaiku] = useState();
@@ -14,49 +12,55 @@ const App = (): JSX.Element => {
     // console.log(haiku);
   };
   return (
-    <>
-      <Container fluid>
-        <Header
-          as="h1"
-          content="Insecure Poetry"
-      // inverted
-          style={{
-            fontSize: mobile ? '2em' : '4em',
-            fontWeight: 'normal',
-            marginBottom: 0,
-            marginTop: mobile ? '0.5em' : '1em',
-          }}
-        />
-        <Header
-          as="h2"
-          content="Bad poetry made from the top 10,000 most commonly used passwords."
-      // inverted
-          style={{
-            fontSize: mobile ? '1.25em' : '1.5em',
-            fontWeight: 'normal',
-            marginTop: mobile ? '0.25em' : '0.75em',
-          }}
-        />
-        <Button
-          primary
-          size="large"
-          onClick={(): void => doHaiku()}
-        >
-          Get A Haiku
-        </Button>
-      </Container>
-      <Container
-        fluid
-        style={{
-          font: 'Kalam',
-          fontSize: mobile ? '1.25em' : '1.5em',
-          fontWeight: 'normal',
-          marginTop: mobile ? '0.5em' : '1.5em',
-        }}
-      >
-        {haiku && haiku.map((line: string | number | undefined) => <p style={{ margin: '0 0 .25em' }} key={line}>{line}</p>)}
-      </Container>
-    </>
+    <Container maxWidth="md">
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography
+            variant="h1"
+            style={{ fontFamily: 'Permanent Marker' }}
+          >
+            Insecure Poetry
+
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h5">
+            Bad poetry made from the most commonly used passwords.
+          </Typography>
+        </Grid>
+        <Grid item sm={2}>
+          <Button
+            style={{ backgroundColor: '#000' }}
+            variant="contained"
+            color="primary"
+            onClick={(): void => doHaiku()}
+          >
+            Get A Haiku
+          </Button>
+        </Grid>
+        <Grid item sm={10}>
+          <Typography>(other styles coming soon...maybe)</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          {haiku && (
+          <Card>
+            <CardContent>
+              {haiku.map((line: string | number | undefined) => (
+                <Typography
+                  style={{
+                    fontFamily: 'Reenie Beanie', fontSize: '3em', lineHeight: '1', marginBottom: '0.3em',
+                  }}
+                  key={line}
+                >
+                  {line}
+                </Typography>
+              ))}
+            </CardContent>
+          </Card>
+          )}
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
